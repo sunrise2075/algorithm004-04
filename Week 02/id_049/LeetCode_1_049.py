2d9370e74c344d2c539c3c39e1cebd70bd41756b
sunrise2075 <sunrise2075@163.com>
2019-12-28 06:48:11 +0800

    清理无关本人的文件夹提交

--- Week 02/id_049/LeetCode_1_049.py
+++
@@ -1,6 +0,0 @@
-def twoSumsHash(self, nums: List[int], target: int) -> List[int]:
-    hashmap = {}
-    for i, v in enumerate(nums):
-        if hashmap.get(target-v) in hashmap:
-            return [hashmap.get(target-v),i]
-    hashmap[v]=i
